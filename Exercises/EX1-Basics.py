3a524907d59100b7695110bc4774b913920f32f8
Myah <myahbowley@gmail.com>
2026-03-16 22:26:50 -0300

    Added EX1-Basics.py file

---
+++ Exercises/EX1-Basics.py
@@ -0,0 +1,22 @@
+# LAB 1 - BASICS
+# Exploring the print command
+
+# 1. Add a command to display the word Hello.
+print("Hello")
+
+# 2. Add a command to display the phrase "I don't like green eggs and ham".
+print("I don't like green eggs and ham")
+
+# 3. Add command(s) to display the lyrics to row, row, row your boat on 4 separate lines.
+print("~Row, row, row your boat~")
+print("~gently down the stream~")
+print("~merrily, merrily, merrily~")
+print("~life is but a dream~")
+
+# 4. Add a command to display the results of 3 + 4 (ie. result of a math operation)
+print(3 + 4)
+
+# 5. Add commands to save the value "fish" to a variable, 
+#    then to display the contents of that variable on screen.
+fishyVariable = "fish"
+print(fishyVariable)
